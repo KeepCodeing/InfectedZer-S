030eece9775dae1a8c4926cf2c8f8447d9193cf5
KeepCoding <41890343+KeepCodeing@users.noreply.github.com>
2018-09-23 18:26:28 +0800

    Update BootsJS.js

--- BootsJS.js
+++ BootsJS.js
@@ -48,10 +48,10 @@ window.onload=function () {
             color:"black"
         });
     })；
-     var mouth=new Date().getMonth();
-        console.log(mouth);
-        $("#mouth").html(mouth+"月...漫无止境");
-        $("#mouthtext").html("while(new Date.getMonth=="+mouth+"){<br>\n" +
-            "                        document.write(\"这是一个没有目的的循环...\");<br>\n" +
-            "                        }");
+      var mouth=new Date().getMonth();
+            var mouth2=mouth+1;
+            $("#mouth").html(mouth2+"月...漫无止境");
+            $("#mouthtext").html("while(new Date.getMonth=="+mouth2+"){<br>\n" +
+                "                        document.write(\"这是一个没有目的的循环...\");<br>\n" +
+                "                        }");
 };
